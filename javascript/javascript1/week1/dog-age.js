bf1279ed6d67b5d3de36664616f0da767f5ff127
zmzade <zmahmoodzadeh@yahoo.com>
2021-05-04 18:03:23 +0200

    dog age calculation done based on dog age or human age

---
+++ javascript/javascript1/week1/dog-age.js
@@ -0,0 +1,9 @@
+const dogYearOfBirth = 2021; // const, as year of birth doesnot change
+let dogYearFuture = 2031;
+let dogYear = dogYearFuture - dogYearOfBirth;
+let shouldShowResultInDogYears = true // a boolean variable
+if (shouldShowResultInDogYears = false) {
+    console.log(`your dog will be ${dogYear} human years old in 2031`)
+} else {
+    console.log(`your dog will be ${7 * dogYear} dog years old in 2031`)
+}
