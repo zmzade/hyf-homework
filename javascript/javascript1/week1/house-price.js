9845d1a222268d0368f5baadfb5373244b82e816
zmzade <zmahmoodzadeh@yahoo.com>
2021-05-04 19:38:07 +0200

    house prices calculated according to formula

---
+++ javascript/javascript1/week1/house-price.js
@@ -0,0 +1,18 @@
+// peter house measures
+let wide = 8;
+let deep= 10;
+let high = 10;
+let gardenSizeInM2 = 100;
+let volumeInMeters = wide * deep * high
+let housePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
+console.log(housePrice)
+// julia house measures
+wide = 5
+deep = 11
+high = 8
+gardenSizeInM2 = 70
+volumeInMeters = wide * deep * high
+housePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
+console.log(housePrice)
+
+
