c99ef6b0e6694b3a59f82104b550fd69c1b7afa1
zmzade <zmahmoodzadeh@yahoo.com>
2021-05-04 20:46:51 +0200

    house prices calculated

--- javascript/javascript1/week1/house-price.js
+++ javascript/javascript1/week1/house-price.js
@@ -6,6 +6,7 @@ let gardenSizeInM2 = 100;
 let volumeInMeters = wide * deep * high
 let housePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
 console.log(housePrice)
+
 // julia house measures
 wide = 5
 deep = 11
