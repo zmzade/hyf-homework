c7d51e25231277f9c5e6ca5a89b6c042ee8e1808
zmzade <zmahmoodzadeh@yahoo.com>
2021-05-04 20:54:33 +0200

    name generater for startup done

---
+++ javascript/javascript1/week1/startup-name-generator.js
@@ -0,0 +1,5 @@
+let firstWords = ['easy', 'awsome', 'cool', 'international', 'new', 'youtube', 'ocean', 'flowers', 'asia', 'Denmark']
+let secondWords = ['giant', 'future', 'corporation', 'human', 'love', 'novels', 'language', 'life', 'far', 'like']
+let startupName;
+startupName = '\"The startup: ' + '\"' + firstWords[1] +  ' ' + secondWords[2]  +'\"' + ' contains 18 characters\"'
+console.log(startupName)
