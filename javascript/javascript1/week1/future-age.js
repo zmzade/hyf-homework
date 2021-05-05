fc6cffddab32cfaebc2690d46948aab1ac3376e6
zmzade <zmahmoodzadeh@yahoo.com>
2021-05-05 22:58:45 +0200

    var age simplified

--- javascript/javascript1/week1/future-age.js
+++ javascript/javascript1/week1/future-age.js
@@ -1,4 +1,4 @@
 const yearOfbirth = 1975
 let yearFuture = 2040
-let age = (2021 - yearOfbirth) + (yearFuture - 2021)
+let age = yearFuture - yearOfbirth
 console.log('you will be '+ age + ' years old in 2040')
