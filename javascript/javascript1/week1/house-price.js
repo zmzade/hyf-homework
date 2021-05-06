c892c1d73f45ac7d4586bf0cab730f7a2af7c71e
zmzade <zmahmoodzadeh@yahoo.com>
2021-05-06 17:54:03 +0200

    answered the question whether Peter or Julia should but the hous

--- javascript/javascript1/week1/house-price.js
+++ javascript/javascript1/week1/house-price.js
@@ -5,7 +5,14 @@ let high = 10;
 let gardenSizeInM2 = 100;
 let volumeInMeters = wide * deep * high
 let housePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
-console.log(housePrice)
+let houseValue = 2500000
+let shouldBuyTheHouse;
+if (houseValue > housePrice) {
+   shouldBuyTheHouse = true
+} else {shouldBuyTheHouse = false}
+console.log(`Peter has to pay: ${housePrice}`)
+console.log(`The value of the house is : ${houseValue}`)
+console.log(`Peter should buy the house: ${shouldBuyTheHouse}`)
 
 // julia house measures
 wide = 5
@@ -14,6 +21,14 @@ high = 8
 gardenSizeInM2 = 70
 volumeInMeters = wide * deep * high
 housePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
-console.log(housePrice)
+houseValue = 1000000
+if (houseValue > housePrice) {
+    shouldBuyTheHouse = true
+}else {
+    shouldBuyTheHouse = false
+}
+console.log(`Julia has to pay: ${housePrice}`)
+console.log(`The value of the house is : ${houseValue}`)
+console.log(`Julia should buy the house: ${shouldBuyTheHouse}`)
 
 
