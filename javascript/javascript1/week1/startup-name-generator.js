c04d15f7dfe45c3972ec26dcacb4b145d77aa401
zmzade <zmahmoodzadeh@yahoo.com>
2021-05-07 15:14:37 +0200

    deletion of 0 at the of Random Number

--- javascript/javascript1/week1/startup-name-generator.js
+++ javascript/javascript1/week1/startup-name-generator.js
@@ -1,8 +1,8 @@
 let firstWords = ['easy', 'awsome', 'cool', 'international', 'new', 'youtube', 'ocean', 'flowers', 'asia', 'Denmark']
 let secondWords = ['giant', 'future', 'corporation', 'human', 'love', 'novels', 'language', 'life', 'far', 'like']
 
-const randomNumber1 = Math.floor(Math.random() * 10) + 0;
-const randomNumber2 = Math.floor(Math.random() * 10) + 0;
+const randomNumber1 = Math.floor(Math.random() * 10);
+const randomNumber2 = Math.floor(Math.random() * 10);
  let sumOfCharacters = firstWords[randomNumber1].length + secondWords[randomNumber2].length + 1
 let startupName = '\"The startup: ' + '\"' + firstWords[randomNumber1] +  ' ' + secondWords[randomNumber2]  +'\"' + ' contains ' +  sumOfCharacters  + ' characters\"'
 console.log(startupName)
