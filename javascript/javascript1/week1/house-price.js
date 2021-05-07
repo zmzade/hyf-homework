3fa2db381d1fb25072cbc9ff94a97a35402ae35b
zmzade <zmahmoodzadeh@yahoo.com>
2021-05-07 15:12:38 +0200

    simplified by storing the boolean value in a variable

--- javascript/javascript1/week1/house-price.js
+++ javascript/javascript1/week1/house-price.js
@@ -4,12 +4,9 @@ let deep= 10;
 let high = 10;
 let gardenSizeInM2 = 100;
 let volumeInMeters = wide * deep * high
-let housePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
-let houseValue = 2500000
-let shouldBuyTheHouse;
-if (houseValue > housePrice) {
-   shouldBuyTheHouse = true
-} else {shouldBuyTheHouse = false}
+let houseValue = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
+let housePrice = 2500000
+let shouldBuyTheHouse = housePrice < houseValue;
 console.log(`Peter has to pay: ${housePrice}`)
 console.log(`The value of the house is : ${houseValue}`)
 console.log(`Peter should buy the house: ${shouldBuyTheHouse}`)
@@ -20,13 +17,9 @@ deep = 11
 high = 8
 gardenSizeInM2 = 70
 volumeInMeters = wide * deep * high
-housePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
-houseValue = 1000000
-if (houseValue > housePrice) {
-    shouldBuyTheHouse = true
-}else {
-    shouldBuyTheHouse = false
-}
+houseValue = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
+housePrice = 1000000
+shouldBuyTheHouse = housePrice < houseValue;
 console.log(`Julia has to pay: ${housePrice}`)
 console.log(`The value of the house is : ${houseValue}`)
 console.log(`Julia should buy the house: ${shouldBuyTheHouse}`)
