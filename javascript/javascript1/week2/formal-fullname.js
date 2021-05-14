a4045346aafc37cb53abcd01006317f8fb269562
zmzade <zmahmoodzadeh@yahoo.com>
2021-05-14 17:08:30 +0200

    reducing if nests

--- javascript/javascript1/week2/formal-fullname.js
+++ javascript/javascript1/week2/formal-fullname.js
@@ -1,22 +1,21 @@
 
 
 function getFullname (firstname, surname, useFormalName, gender) {
- if (firstname && surname)  {
-    if (useFormalName) {
-        if (gender === 'male') {
-            return (`"Lord ${firstname} ${surname}"`)
-        }
-        else if (gender === 'female') {
-            return (`"Lady ${firstname} ${surname}"`)
-        }
-    } else {
-        return (`"${firstname} ${surname}"`)
+    if (!firstname || !surname)  {
+        return `"Please fill up firstname and surname."`
     }
- } else {
-     return (`"Please fill up firstname and surname."`)
- }
-    
- }
+    else {
+    if (useFormalName && gender === 'male') {
+        return `"Lord ${firstname} ${surname}"`
+    }
+    else if (useFormalName && gender === 'female') {
+        return `"Lady ${firstname} ${surname}"`
+    } 
+    else {
+        return `"${firstname} ${surname}"`
+    }
+    } 
+}
    
    console.log(getFullname("Benjamin", "Hughes", true, 'male'));
    console.log(getFullname("Adrian", "Hughes", "", 'male'));
--- javascript/javascript1/week2/student-manager.js
+++ javascript/javascript1/week2/student-manager.js
@@ -1,20 +1,21 @@
 
 const class07Students = [];
 function addStudentToClass(studentName) {
-    if (studentName) {
-        if (class07Students.length < 6) {
-            if (class07Students.includes(studentName)) {
-                console.log(`Student ${studentName} is already in the class`)
-                }
-            else {
-                return class07Students.push(studentName)
-                } 
-        }else if (studentName === 'Queen'){
-            return class07Students.push('Queen')
-        }else {
-            console.log('Cannot add more students to class 07') 
-            }
+    if (!studentName) {
+        console.log(`"Please write down your name"`)
+        return
+    }else {
+    if (class07Students.length < 6 || studentName === 'Queen') {
+        if (class07Students.includes(studentName)) {
+            console.log(`Student ${studentName} is already in the class`)
         }
+        else {
+            return class07Students.push(studentName)
+        } 
+    }else {
+        console.log('Cannot add more students to class 07') 
+    }
+}
    
 }
 
@@ -51,6 +52,9 @@ addStudentToClass('Rune')
 console.log(class07Students)
 console.log(getNumberOfStudents())
 addStudentToClass('Mary')
+console.log(class07Students)
+console.log(getNumberOfStudents())
+addStudentToClass('Queen') // add queen to a full class
 console.log(class07Students)
 console.log(getNumberOfStudents())
 addStudentToClass('Queen')
