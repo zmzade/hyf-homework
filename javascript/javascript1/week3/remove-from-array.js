ceebf068af863686755497c05ffd03542ecbf77d
zmzade <zmahmoodzadeh@yahoo.com>
2021-05-18 15:41:39 +0200

    remove item done

--- javascript/javascript1/week3/remove-from-array.js
+++ javascript/javascript1/week3/remove-from-array.js
@@ -9,9 +9,10 @@ const names = [
     "Tala",
   ];
   const nameToRemove = "Ahmad";
-  
-  names.splice(1, 1);
-  
+  names.splice(names.indexOf(nameToRemove), 1)
+  //names.splice(1, 1);
+  //let result = names.filter (name => name !== nameToRemove)
   // Code done
   
   console.log(names); // ['Peter', 'Yana', 'kristina', 'Rasmus', 'Samuel', 'katrine', 'Tala']
+  //console.log (result)
