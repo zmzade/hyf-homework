2072125c71aa55ed4577915967f2e85d714e43b6
zmzade <zmahmoodzadeh@yahoo.com>
2021-05-25 20:17:57 +0200

    use Prettier

--- javascript/javascript1/week3/smartPhone-app.js
+++ javascript/javascript1/week3/smartPhone-app.js
@@ -1,25 +1,25 @@
 // first part
-let activities = []
-function addActivity (date, activity, duration){
-
-    if (typeof date === 'string' && typeof activity === 'string' && typeof duration === 'number'){
-        
-        let object = {date, activity, duration}
-        activities.push(object)
-    }
-    
+let activities = [];
+function addActivity(date, activity, duration) {
+  if (
+    typeof date === "string" &&
+    typeof activity === "string" &&
+    typeof duration === "number"
+  ) {
+    let object = { date, activity, duration };
+    activities.push(object);
+  }
 }
 
 addActivity("23/7-18", "Youtube", 30);
 addActivity("23/7-18", "Youtube", 30);
 addActivity("23/7-18", "Youtube", 3);
-addActivity(2, '', 'I');
+addActivity(2, "", "I");
 addActivity("30/7-18", "facebook", 20);
 addActivity("1/3-21", "instagram", 15);
 addActivity("1/3-21", "instagram", 40);
 addActivity("1/3-21", "instagram", 50);
-console.log(activities)
-
+console.log(activities);
 
 /*
 activities should now look like this
@@ -32,65 +32,67 @@ activities should now look like this
 
 // second part
 
-function showStatus (activities, timeLimit){
-    if (activities.length !== 0){
-        
-        let totalTime = 0;
-            for (let i = 0; i < activities.length; i++){
-                let time = activities[i].duration;
-                    totalTime += time
-            }
-                console.log(`You have added ${activities.length} activities. They amount to ${totalTime} min. of usage`)
-                    if (timeLimit <= totalTime){
-                         console.log(`You have reached your limit, no more smartphoning for you!`)    
-                    }   
-        return
-    }     
-    console.log(`Add some activities before calling showStatus`)
+function showStatus(activities, timeLimit) {
+  if (activities.length !== 0) {
+    let totalTime = 0;
+    for (let i = 0; i < activities.length; i++) {
+      let time = activities[i].duration;
+      totalTime += time;
+    }
+    console.log(
+      `You have added ${activities.length} activities. They amount to ${totalTime} min. of usage`
+    );
+    if (timeLimit <= totalTime) {
+      console.log(`You have reached your limit, no more smartphoning for you!`);
+    }
+    return;
+  }
+  console.log(`Add some activities before calling showStatus`);
 }
 
 showStatus(activities); // will log out this "You have added 3 activities. They amount to 78 min. of usage"
-showStatus('', 0);//in case activities is empty log out "Add some activities before calling showStatus"
-showStatus(activities,60); //"You have reached your limit, no more smartphoning for you!"
+showStatus("", 0); //in case activities is empty log out "Add some activities before calling showStatus"
+showStatus(activities, 60); //"You have reached your limit, no more smartphoning for you!"
 showStatus(activities, 200); // will log out this "You have added 3 activities. They amount to 78 min. of usage"
 
-
 //Create a function for calculating the activity a user has spent the most time on.
-let totalTimeActivity= [] 
+let totalTimeActivity = [];
 
-function calculateMostUsedActivity(activities){
-    let check = [], uniqueActivities = [];
+function calculateMostUsedActivity(activities) {
+  let check = [],
+    uniqueActivities = [];
 
-    for(let i = 0; i < activities.length; i++){
-        const { activity} = activities[i];//object destructuring
-        if(!uniqueActivities.includes(activity)) {
-        uniqueActivities.push(activity)
-        }
-        /*
+  for (let i = 0; i < activities.length; i++) {
+    const { activity } = activities[i]; //object destructuring
+    if (!uniqueActivities.includes(activity)) {
+      uniqueActivities.push(activity);
+    }
+    /*
         if( check[activities[i].activity]){
             continue;}
                 check[activities[i].activity] = true;
                     uniqueActivity.push(activities[i].activity);
-        */           
+        */
+  }
+  for (let j = 0; j < uniqueActivities.length; j++) {
+    let totalDuration = 0;
+    for (let k = 0; k < activities.length; k++) {
+      if (activities[k].activity === uniqueActivities[j]) {
+        totalDuration += activities[k].duration;
+      }
     }
-    for(let j =0; j<uniqueActivities.length; j++){
-        let totalDuration = 0;
-            for(let k=0; k<activities.length; k++){
-                if(activities[k].activity === uniqueActivities[j]){
-                    totalDuration+=activities[k].duration
-                }
-            }
-        let activityWithDuration = {name:uniqueActivities[j], totalDuration}
-            totalTimeActivity.push(activityWithDuration); 
+    let activityWithDuration = { name: uniqueActivities[j], totalDuration };
+    totalTimeActivity.push(activityWithDuration);
+  }
+  console.log(totalTimeActivity);
+  let maxDuration = 0,
+    maxAcitivty = "";
+  for (let i = 0; i < totalTimeActivity.length; i++) {
+    if (totalTimeActivity[i].totalDuration > maxDuration) {
+      maxDuration = totalTimeActivity[i].totalDuration;
+      maxAcitivty = totalTimeActivity[i].name;
     }
-    console.log(totalTimeActivity)
-    let maxDuration = 0, maxAcitivty='';
-    for (let i = 0; i < totalTimeActivity.length; i++){
-            if (totalTimeActivity[i].totalDuration > maxDuration){
-                maxDuration = totalTimeActivity[i].totalDuration;
-                maxAcitivty = totalTimeActivity[i].name
-        }
-    }
-    console.log(`most activity is on: ${maxAcitivty} with ${maxDuration} min.`)
+  }
+  console.log(`most activity is on: ${maxAcitivty} with ${maxDuration} min.`);
 }
-calculateMostUsedActivity(activities)
+calculateMostUsedActivity(activities);
--- javascript/javascript1/week3/speed-function.js
+++ javascript/javascript1/week3/speed-function.js
@@ -1,13 +1,13 @@
 const travelInformation = {
-    speed: 50,
-    destinationDistance: 432,
+  speed: 50,
+  destinationDistance: 432,
 };
-  
-function howlongTakes (travelInformation){ 
-    const hours = travelInformation.destinationDistance / travelInformation.speed
-    const minutes = (hours - Math.floor(hours)) * 60
-    return `${Math.floor (hours)} hours and ${minutes.toFixed(0)} minutes`
+
+function howlongTakes(travelInformation) {
+  const hours = travelInformation.destinationDistance / travelInformation.speed;
+  const minutes = (hours - Math.floor(hours)) * 60;
+  return `${Math.floor(hours)} hours and ${minutes.toFixed(0)} minutes`;
 }
 
 const travelTime = howlongTakes(travelInformation);
-console.log(travelTime); 
+console.log(travelTime);
