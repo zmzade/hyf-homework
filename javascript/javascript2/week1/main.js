0506b827e8535ee91640918a3d8c563c38e6d89f
zmzade <zmahmoodzadeh@yahoo.com>
2021-06-01 14:54:09 +0200

    hyfBayHelper done

---
+++ javascript/javascript2/week1/main.js
@@ -0,0 +1,18 @@
+//console.log("Script loaded");
+
+//const products = getAvailableProducts();
+//console.log(products);
+
+const products = getAvailableProducts();
+
+function renderProducts(products) {
+  // your code here
+  const ul = document.querySelector("ul");
+  for (let i = 0; i < products.length; i++) {
+    let li = document.createElement("li");
+    li.innerHTML = `<h2>${products[i].name}</h2><span>price: ${products[i].price}</span><br><span>Rating: ${products[i].rating}</span>`;
+    ul.appendChild(li);
+  }
+}
+
+renderProducts(products); // This should create the ul and the li's with the individual products details
