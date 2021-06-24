134b42735caf95c59bc0142b38ae30026336aff8
zmzade <zmahmoodzadeh@yahoo.com>
2021-06-24 10:26:38 +0200

    done giphy app, problem with limit part

---
+++ javascript/javascript3/week1/giphy.js
@@ -0,0 +1,36 @@
+const apiKey = "kBxVzpkU6jDfdPzWItFmlZNSjDsyZ6Jy";
+const button = document.getElementById("button");
+const gifDiv = document.getElementById("gifDiv");
+const input = document.getElementById("input-limit");
+
+input.addEventListener("input", function (event) {
+  let userLimit = event.target.value;
+  renderGifs();
+});
+
+button.addEventListener("click", renderGifs);
+
+function renderGifs() {
+  let userWord = document.getElementById("input-text").value;
+  userLimit = input.value;
+  console.log(userLimit);
+
+  if (!userWord) {
+    alert("please insert the search word");
+  }
+  if (!userLimit) {
+    userLimit = 10;
+  }
+
+  fetch(
+    `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${userWord}&limit=${userLimit}&offset=0&rating=g&lang=en`
+  )
+    .then((res) => res.json())
+    .then((result) => {
+      const gifs = result.data.map((entry) => {
+        const image = document.createElement("img");
+        image.setAttribute("src", entry.images.downsized.url);
+        gifDiv.appendChild(image);
+      });
+    });
+}
