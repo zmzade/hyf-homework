b33306d55ba91459e6bb1804fb8515c4181d189b
zmzade <zmahmoodzadeh@yahoo.com>
2021-07-01 14:40:13 +0200

    added editions

--- javascript/javascript2/week3/fastest-presser.js
+++ javascript/javascript2/week3/fastest-presser.js
@@ -7,27 +7,28 @@ const gameButton = document.querySelector("#start"); //start game button
 const input = document.querySelector("input");
 let sCount = 0;
 let lCount = 0;
+let confetti = [];
 
 const resultTag = document.getElementById("game-result"); //game result
 resultTag.innerHTML = "";
 const gameResult = () => {
-  if (sCount == 0 && lCount == 0) {
+  if (sCount === 0 && lCount === 0) {
     resultTag.innerHTML = "No action!!!";
   }
-  if (sCount == lCount && sCount > 0 && lCount > 0) {
+  if (sCount === lCount && sCount > 0 && lCount > 0) {
     resultTag.innerHTML = "Game Draw!!!";
   }
   if (sCount > lCount) {
     resultTag.innerHTML = "S is the winer !!!";
 
     const confettiSettings = { target: "s-canvas", size: 2 };
-    const confetti = new ConfettiGenerator(confettiSettings);
+    confetti = new ConfettiGenerator(confettiSettings);
     confetti.render();
   }
   if (lCount > sCount) {
     resultTag.innerHTML = "L is the winner";
     const confettiSettings = { target: "l-canvas", size: 2 };
-    const confetti = new ConfettiGenerator(confettiSettings);
+    confetti = new ConfettiGenerator(confettiSettings);
     confetti.render();
   }
 };
@@ -35,7 +36,7 @@ const gameResult = () => {
 gameButton.addEventListener("click", gameStart);
 function gameStart() {
   let gameTime = input.value;
-  if (gameTime == "") {
+  if (gameTime === "") {
     alert("please enter time");
     document.removeEventListener("keydown", keyDetect);
   }
@@ -50,11 +51,11 @@ function gameStart() {
     document.addEventListener("keydown", keyDetect); //detect the keys
     function keyDetect(event) {
       let userKey = event.key;
-      if (userKey == "s") {
+      if (userKey === "s") {
         sCount++;
         document.getElementById("S-press").innerHTML = sCount;
       }
-      if (userKey == "l") {
+      if (userKey === "l") {
         lCount++;
         document.getElementById("L-press").innerHTML = lCount;
       }
