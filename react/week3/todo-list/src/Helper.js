4329d0897439e7a8ae31cf8f1ef9bce4b4967c80
zmzade <zmahmoodzadeh@yahoo.com>
2021-10-20 21:40:28 +0200

    edited Helper comp

--- react/week3/todo-list/src/Helper.js
+++ react/week3/todo-list/src/Helper.js
@@ -1,7 +1,6 @@
 const getNextId = (todos) => {
   const lastTodo = todos[todos.length - 1];
   const nextId = lastTodo === undefined ? 1 : lastTodo.id + 1;
-  //const nextId = Math.max(...todos.map((item) => item.id)) + 1;
   return nextId;
 };
 
